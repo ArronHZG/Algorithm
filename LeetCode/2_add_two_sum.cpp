cefb89a40918a2b3669be362778e622782cac0c2
houzhenggangGit <hou.zg@foxmail.com>
2019-04-27 04:02:57 +0800

    优化代码

---
+++ LeetCode/2_add_two_sum.cpp
@@ -0,0 +1,24 @@
+//
+// Created by arron on 2019/4/27.
+//
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+
+    }
+};
+
