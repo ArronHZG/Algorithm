f521dcc0feb5e4f0a4493557020670485db0399a
ArronHZG <hou.zg@foxmail.com>
2020-02-03 19:09:42 +0800

    - [x] 1. 求开方   - [x] 2. 大于给定元素的最小元素   - [x] 3. 有序数组的 Single Element   - [x] 4. 第一个错误的版本   - [x] 5. 旋转数组的最小数字

---
+++ LeetCode/BinarySearch/153-find-minimum-in-rotated-sorted-array.cpp
@@ -0,0 +1,115 @@
+//
+// Created by 侯正罡 on 2020/2/3.
+//
+
+/**
+假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+
+( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+
+请找出其中最小的元素。
+
+你可以假设数组中不存在重复元素。
+
+示例 1:
+
+输入: [3,4,5,1,2]
+输出: 1
+示例 2:
+
+输入: [4,5,6,7,0,1,2]
+输出: 0
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+#include <iostream>
+#include <vector>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int> &nums) {
+        // initializing left and right pointers.
+        int len = nums.size();
+        int left = 0;
+        int right = len - 1;
+        if (len == 1 || nums[right] > nums[0]) {
+            return nums[0];
+        }
+        while (right >= left) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            }
+            if (nums[mid - 1] > nums[mid]) {
+                return nums[mid];
+            }
+            if (nums[mid] > nums[0]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+};
+
+TEST(findMinimumInRotatedSortedArray, 1) { /* NOLINT */
+    cout << endl;
+    vector<int> input = {3, 4, 5, 1, 2};
+    auto answer = 1;
+    auto output = Solution().findMin(input);
+    EXPECT_EQ(answer, output);
+}
+
+TEST(findMinimumInRotatedSortedArray, 2) { /* NOLINT */
+    cout << endl;
+    vector<int> input = {4, 5, 6, 7, 0, 1, 2};
+    auto answer = 0;
+    auto output = Solution().findMin(input);
+    EXPECT_EQ(answer, output);
+}
+
+TEST(findMinimumInRotatedSortedArray, 3) { /* NOLINT */
+    cout << endl;
+    vector<int> input = {0, 1, 2};
+    auto answer = 0;
+    auto output = Solution().findMin(input);
+    EXPECT_EQ(answer, output);
+}
+
+TEST(findMinimumInRotatedSortedArray, 4) { /* NOLINT */
+    cout << endl;
+    vector<int> input = {1};
+    auto answer = 1;
+    auto output = Solution().findMin(input);
+    EXPECT_EQ(answer, output);
+}
+
+TEST(findMinimumInRotatedSortedArray, 5) { /* NOLINT */
+    cout << endl;
+    vector<int> input = {2, 1};
+    auto answer = 1;
+    auto output = Solution().findMin(input);
+    EXPECT_EQ(answer, output);
+}
+
+TEST(findMinimumInRotatedSortedArray, 6) { /* NOLINT */
+    cout << endl;
+    vector<int> input = {3, 1, 2};
+    auto answer = 1;
+    auto output = Solution().findMin(input);
+    EXPECT_EQ(answer, output);
+}
+
+TEST(findMinimumInRotatedSortedArray, 7) { /* NOLINT */
+    cout << endl;
+    vector<int> input = {5, 1, 2, 3, 4};
+    auto answer = 1;
+    auto output = Solution().findMin(input);
+    EXPECT_EQ(answer, output);
+}
---
+++ LeetCode/BinarySearch/540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,88 @@
+//
+// Created by 侯正罡 on 2020/2/3.
+//
+
+/**
+给定一个只包含整数的有序数组，每个元素都会出现两次，唯有一个数只会出现一次，找出这个数。
+
+示例 1:
+
+输入: [1,1,2,3,3,4,4,8,8]
+输出: 2
+示例 2:
+
+输入: [3,3,7,7,10,11,11]
+输出: 10
+注意: 您的方案应该在 O(log n)时间复杂度和 O(1)空间复杂度中运行。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/single-element-in-a-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+#include <iostream>
+#include <vector>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int> &nums) {
+        return single(nums, 0, nums.size() - 1);
+
+    }
+
+    int single(vector<int> &nums, int left, int right) {
+        if (left == right) {
+            return nums[left];
+        }
+        int mid = left + (right - left) / 2;
+        if (nums[mid - 1] == nums[mid]) {
+            if ((mid - 2 - left + 1) % 2 == 1) {
+                return single(nums, left, mid - 2);
+            } else {
+                return single(nums, mid + 1, right);
+            }
+        } else if (nums[mid] == nums[mid + 1]) {
+            if ((mid - 1 - left + 1) % 2 == 1) {
+                return single(nums, left, mid - 1);
+            } else {
+                return single(nums, mid + 2, right);
+            }
+        } else
+            return single(nums, mid, mid);
+    }
+};
+
+TEST(singleNonDuplicate, 1) { /* NOLINT */
+    cout << endl;
+    vector<int> input{1, 1, 2, 3, 3, 4, 4, 8, 8};
+    auto answer = 2;
+    auto output = Solution().singleNonDuplicate(input);
+    EXPECT_EQ(answer, output);
+}
+
+TEST(singleNonDuplicate, 2) { /* NOLINT */
+    cout << endl;
+    vector<int> input{3, 3, 7, 7, 10, 11, 11};
+    auto answer = 10;
+    auto output = Solution().singleNonDuplicate(input);
+    EXPECT_EQ(answer, output);
+}
+
+TEST(singleNonDuplicate, 3) { /* NOLINT */
+    cout << endl;
+    vector<int> input{1};
+    auto answer = 1;
+    auto output = Solution().singleNonDuplicate(input);
+    EXPECT_EQ(answer, output);
+}
+
+TEST(singleNonDuplicate, 4) { /* NOLINT */
+    cout << endl;
+    vector<int> input{1, 1, 2};
+    auto answer = 2;
+    auto output = Solution().singleNonDuplicate(input);
+    EXPECT_EQ(answer, output);
+}
