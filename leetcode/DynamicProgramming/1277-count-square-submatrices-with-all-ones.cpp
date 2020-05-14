b996fb1af007e8743ba4ead080dfa5c68a39ba5b
ArronHZG <hou.zg@foxmail.com>
2020-05-14 23:13:45 +0800

    countSquares

--- leetcode/DynamicProgramming/1277-count-square-submatrices-with-all-ones.cpp
+++ leetcode/DynamicProgramming/1277-count-square-submatrices-with-all-ones.cpp
@@ -10,15 +10,34 @@ using namespace std;
 
 class Solution {
 public:
-    int countSquares(vector<int> &nums) {
-        return 0;
+    int countSquares(vector<vector<int>> &matrix) {
+        if (matrix.empty() || matrix.back().empty()) return 0;
+        int rows = matrix.size(), columns = matrix.back().size();
+        vector<vector<int>> dp(rows, vector<int>(columns));
+        int count = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == 1) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                }
+                count += dp[i][j];
+            }
+        }
+        return count;
     }
 };
 
 TEST(countSquares, 1) {
     cout << endl;
-    vector<int> input = {};
-    auto answer = 0;
+    vector<vector<int>> input = {{1, 0, 1, 0, 0},
+                                 {1, 0, 1, 1, 1},
+                                 {1, 1, 1, 1, 1},
+                                 {1, 0, 0, 1, 0}};
+    auto answer = 15;
     auto output = Solution().countSquares(input);
     EXPECT_EQ(answer, output);
 }
--- leetcode/DynamicProgramming/221-maximal-square.cpp
+++ leetcode/DynamicProgramming/221-maximal-square.cpp
@@ -10,15 +10,34 @@ using namespace std;
 
 class Solution {
 public:
-    int maximalSquare(vector<int> &nums) {
-        return 0;
+    int maximalSquare(vector<vector<char>> &matrix) {
+        if (matrix.empty() || matrix.back().empty()) return 0;
+        int rows = matrix.size(), columns = matrix.back().size();
+        int maxSide = 0;
+        vector<vector<int>> dp(rows, vector<int>(columns));
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = max(maxSide, dp[i][j]);
+                }
+            }
+        }
+        return maxSide * maxSide;;
     }
 };
 
 TEST(maximalSquare, 1) {
     cout << endl;
-    vector<int> input = {};
-    auto answer = 0;
+    vector<vector<char>> input = {{'1', '0', '1', '0', '0'},
+                                  {'1', '0', '1', '1', '1'},
+                                  {'1', '1', '1', '1', '1'},
+                                  {'1', '0', '0', '1', '0'}};
+    auto answer = 4;
     auto output = Solution().maximalSquare(input);
     EXPECT_EQ(answer, output);
 }
